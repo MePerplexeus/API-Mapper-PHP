526274ca4eb7bcd3d4b1eab9d9e714135aec19d6
PerplexeusWS <me.perplexeus@gmail.com>
2022-08-09 00:03:26 +0530

    [FIX] QuoteOfTheDay Format

--- v1/functions.php
+++ v1/functions.php
@@ -6,7 +6,15 @@ function todaysQuote() {
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     $output = curl_exec($curl);
     curl_close($curl);
-    return json_encode(json_decode($output)[0]);
+    
+    $myObj = [
+        "status" => http_response_text(http_response_code()),
+        "code" => http_response_code(),
+        "message" => "Here's Your Quote Of The Day!",
+        "quote" => json_decode($output)[0],
+        "http_origin" => (isset($_SERVER['HTTP_REFERER'])) ? $_SERVER['HTTP_REFERER'] : null,
+    ];
+    return json_encode($myObj);
 }
 
 ?>
