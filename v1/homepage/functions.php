058d0c9de642c3131560e1a06d8fd4200107a852
ArchPerplexeus <me.perplexeus@gmail.com>
2022-08-09 18:15:06 +0530

    [ADD] Banners Support

--- v1/homepage/functions.php
+++ v1/homepage/functions.php
@@ -5,6 +5,21 @@ function get_banners() {
     $auth = authorization_check();
     if ($auth[0]) {
 
+        // required input data
+        // => page (homepage/userdash)
+        // => status (published/draft/inactive)
+        // => date (today)
+
+        // content DB
+        // => id
+        // => type [banner_homepage/banner_userdash/posts(blog)/shorts/reviews/...]
+        // => content [banner(bg[],heading,subheading,button[])/...]
+        // => status
+        // => date_created
+        // => created_by
+        // => start_date
+        // => end_date
+
         $banners = [
             [
                 "bg" => [
@@ -13,10 +28,38 @@ function get_banners() {
                     "desktop" => "https://www.freepik.com/vectors/background-banner",
                 ],
                 "heading" => "Anime - The source of my motivation",
-                "subheading" => "Anime - The source of my motivation",
+                "subheading" => "Journey to fitness",
+                "button" => [
+                    "text" => "Explore",
+                    "link" => "/anime-fitness",
+                ],
             ],
-            'https://www.freepik.com/vectors/background-banner',
-            'https://www.freepik.com/vectors/background-banner',
+            [
+                "bg" => [
+                    "mobile" => "https://www.freepik.com/vectors/background-banner",
+                    "tablet" => "https://www.freepik.com/vectors/background-banner",
+                    "desktop" => "https://www.freepik.com/vectors/background-banner",
+                ],
+                "heading" => "Anime - The source of my motivation",
+                "subheading" => "Journey to fitness",
+                "button" => [
+                    "text" => "Explore",
+                    "link" => "/anime-fitness",
+                ],
+            ],
+            [
+                "bg" => [
+                    "mobile" => "https://www.freepik.com/vectors/background-banner",
+                    "tablet" => "https://www.freepik.com/vectors/background-banner",
+                    "desktop" => "https://www.freepik.com/vectors/background-banner",
+                ],
+                "heading" => "Anime - The source of my motivation",
+                "subheading" => "Journey to fitness",
+                "button" => [
+                    "text" => "Explore",
+                    "link" => "/anime-fitness",
+                ],
+            ],
         ];
         
         $myObj = [
